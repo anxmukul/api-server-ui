535c6f6a739888db41ba4afbc1f4194682d4388b
Mukul <kmukul1197@gmail.com>
2020-10-08 12:48:52 +0530

    Giving update option

--- src/ShowTodos.js
+++ src/ShowTodos.js
@@ -1,41 +1,57 @@
 import React from 'react';
+import UpdateTodos from './UpdateTodos';
 
 class ShowTodos extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { todos: ['blah'], isFetchComplete: false };
+    this.state = { todos: ['blah'], isFetchComplete: false, updateRequest: false, todoID: -1, todoMessage: '', todoTime: '' };
   }
-
 
   fetchData = () => {
     this.setState({ todos: 'Fetching', isFetchComplete: false });
     console.log('blahblah')
-    fetch('https://guarded-taiga-87327.herokuapp.com/todo')
+    fetch('http://localhost:5000/todo')
       .then(response => response.json())
       .then(data => { console.log(data); this.setState({ todos: data, isFetchComplete: true }) })
       .catch(err => { this.setState({ todos: 'Failed' }) });
     console.log('bloopbloop')
 
   }
+  updateTodo = (arg, arg1, arg2) => {
+    console.log('Inside update Component', arg);
+    this.setState({ updateRequest: true, todoID: arg, todoMessage: arg1, todoTime: arg2});
+    console.log(this.state.todoID);
 
-  getTableData = () => {
+  }
+
+  getTableData = (err, data) => {
     return this.state.todos.map((todo) => {
-      return <tr><td>{todo.message}</td><td>{todo.time}</td></tr>
+      return (<tr><td>{todo.message}</td>
+        <td>{todo.time}</td>
+        <td><button onClick={() => this.updateTodo(todo.todo_id, todo.message, todo.time)}>update</button></td></tr>)
     })
   }
 
   render() {
     console.log('called', this.state)
+    if (this.state.updateRequest) {
+      console.log(this.state);
+      return (
+        <div>
+          <UpdateTodos id={this.state.todoID} time={this.state.todoTime} message = {this.state.todoMessage}/>
+        </div>
+      )
+    }
     if (this.state.isFetchComplete) {
       return (
-      <div className="Table"><table id='todos'>
-        <h1>My Todos</h1>
-        <tbody>
-          {this.getTableData()}
-        </tbody>
-      </table > 
-      <button onClick={this.fetchData} className = "Button">Show Todos</button>
-      </div>)
+        <div className="Table"><table id='todos'>
+          <h1>My Todos</h1>
+          <tbody>
+            {this.getTableData()}
+          </tbody>
+        </table >
+          <button onClick={this.fetchData} className="Button">Show Todos</button>
+        </div>)
     }
     else {
       console.log('in else')
--- src/UpdateTodos.js
+++ src/UpdateTodos.js
@@ -1,30 +1,31 @@
 import React from 'react';
-
-class CreateTodos extends React.Component {
+class UpdateTodos extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { message: '', time: '', isPosted: false };
+        this.state = { message: this.props.message, time: this.props.time, isUpdated: false, showTodo: false }
         this.handleChangeMessage = this.handleChangeMessage.bind(this);
         this.handleChangeTime = this.handleChangeTime.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
-
     handleChangeMessage(event) {
+        console.log('sfdfsf')
         this.setState({ message: event.target.value });
     }
 
     handleChangeTime(event) {
+        console.log('sfdfsf')
         this.setState({ time: event.target.value });
 
     }
-
+    handleCreateClick = () => {
+        this.setState({ showTodo: true });
+    }
     handleSubmit(event) {
         console.log('value  was submitted: ');
         console.log(this.state)
         event.preventDefault();
-        fetch('https://guarded-taiga-87327.herokuapp.com/todo', {
-
-            method: 'POST',
+        fetch(`http://localhost:5000/todo/${this.props.id}`, {
+            method: 'PUT',
             headers: {
                 'Content-Type': 'application/json',
             },
@@ -33,46 +34,50 @@ class CreateTodos extends React.Component {
             .then(response => response.json())
             .then(data => {
                 console.log('Success:', data);
-                this.setState({ isPosted: true });
+                this.setState({ isUpdated: true });
             });
     }
-
-    handleCreateClick = () => {
-        this.setState({message: '', time: '', isPosted: false})
-    }
-
     render() {
-        if (this.state.isPosted) {
-            console.log('again in render')
+        console.log('Inside render of update todos')
+        if (this.state.showTodo) {
             return (
-                <div className = "After-creation">
-                    <h3>ceated</h3>
-                    <h4>Time: {JSON.stringify(this.state.time)}</h4>
-                    <h4>Message: {JSON.stringify(this.state.message)}</h4>
-                    <button onClick={ this.handleCreateClick }>Create Todos</button>
+                <div className = "Show-after-update">
+                    <table>
+                        <tr><td>{this.state.message}</td>
+                            <td>{this.state.time}</td>
+                        </tr>
+                    </table>
                 </div>
             )
         }
-        else {
+        if (this.state.isUpdated) {
             return (
-                <div className = "After-creation">
-                    <form onSubmit={this.handleSubmit}>
-                    <h2>Create new todo</h2>
+                <div className="After-updation">
+                    <h3>Updated</h3>
+                    <h4>Time: {JSON.stringify(this.state.time)}</h4>
+                    <h4>Message: {JSON.stringify(this.state.message)}</h4>
+                    <button onClick={this.handleCreateClick}>Show Todos</button>
+                </div>
+            )
+        }
+        return (
+            <div className="Update">
+                <h3>Your todo id {this.props.id}</h3>
+                <form onSubmit={this.handleSubmit}>
+                    <h2>Update todo</h2>
                     <label>
                         Message:
                     <input type="text" value={this.state.message} onChange={this.handleChangeMessage} />
-                    <br></br>
+                        <br></br>
                     Time:
                       <input type="text" value={this.state.time} onChange={this.handleChangeTime}></input>
                     </label>
                     <br></br>
-                    <input type="submit" value="Submit" />
+                    <input type="submit" value="Update" />
                 </form>
-                </div>
-               
-            );
-        }
+            </div>
+
+        )
     }
 }
-
-export default CreateTodos;
+export default UpdateTodos;
