21cba96afdd02dec683292af6a07660a44148e56
Mukul <kmukul1197@gmail.com>
2020-10-17 11:26:14 +0530

    Add routes for login and signup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
 import React from 'react';
-import ShowTodos from './ShowTodos';
-import CreateTodos from './CreateTodos';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import UsersSignup from './UsersSignup';
 import UsersLogin from './UsersLogin'
+import Home from './Home'
 
 function App() {
   return (
-    <div className="App">
-      {/* <ShowTodos />
-      <CreateTodos /> */}
-      <UsersSignup />
-      <UsersLogin />
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/login">
+            <UsersLogin />
+          </Route>
+          <Route path="/signup">
+            <UsersSignup />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
